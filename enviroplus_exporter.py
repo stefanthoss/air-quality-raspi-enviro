5a934dcedd32482290a04aa045708547943aa2dd
Stefan Thoss <stefanthoss@users.noreply.github.com>
2021-07-23 21:35:00 -0700

    Use black

--- enviroplus_exporter.py
+++ enviroplus_exporter.py
@@ -8,14 +8,13 @@ import time
 from threading import Thread
 
 import requests
-from influxdb_client import InfluxDBClient, Point
-from influxdb_client.client.write_api import SYNCHRONOUS
-from prometheus_client import Gauge, Histogram, start_http_server
-
 from bme280 import BME280
 from enviroplus import gas
+from influxdb_client import InfluxDBClient, Point
+from influxdb_client.client.write_api import SYNCHRONOUS
 from pms5003 import PMS5003
 from pms5003 import ReadTimeoutError as pmsReadTimeoutError
+from prometheus_client import Gauge, Histogram, start_http_server
 
 try:
     from smbus2 import SMBus
@@ -37,11 +36,13 @@ logging.basicConfig(
     datefmt="%Y-%m-%d %H:%M:%S",
 )
 
-logging.info("""enviroplus_exporter.py - Expose readings from the Enviro+ sensor by Pimoroni in Prometheus format
+logging.info(
+    """enviroplus_exporter.py - Expose readings from the Enviro+ sensor by Pimoroni in Prometheus format
 
 Press Ctrl+C to exit!
 
-""")
+"""
+)
 
 DEBUG = os.getenv("DEBUG", "false") == "true"
 
@@ -53,21 +54,112 @@ TEMPERATURE = Gauge("temperature", "Temperature measured (*C)")
 PRESSURE = Gauge("pressure", "Pressure measured (hPa)")
 HUMIDITY = Gauge("humidity", "Relative humidity measured (%)")
 OXIDISING = Gauge("oxidising", "Mostly nitrogen dioxide but could include NO and Hydrogen (Ohms)")
-REDUCING = Gauge("reducing", "Mostly carbon monoxide but could include H2S, Ammonia, Ethanol, Hydrogen, Methane, Propane, Iso-butane (Ohms)")
+REDUCING = Gauge(
+    "reducing",
+    "Mostly carbon monoxide but could include H2S, Ammonia, Ethanol, Hydrogen, Methane, Propane, Iso-butane (Ohms)",
+)
 NH3 = Gauge("NH3", "mostly Ammonia but could also include Hydrogen, Ethanol, Propane, Iso-butane (Ohms)")
 LUX = Gauge("lux", "current ambient light level (lux)")
 PROXIMITY = Gauge("proximity", "proximity, with larger numbers being closer proximity and vice versa")
 PM1 = Gauge("PM1", "Particulate Matter of diameter less than 1 micron. Measured in micrograms per cubic metre (ug/m3)")
-PM25 = Gauge("PM25", "Particulate Matter of diameter less than 2.5 microns. Measured in micrograms per cubic metre (ug/m3)")
-PM10 = Gauge("PM10", "Particulate Matter of diameter less than 10 microns. Measured in micrograms per cubic metre (ug/m3)")
+PM25 = Gauge(
+    "PM25", "Particulate Matter of diameter less than 2.5 microns. Measured in micrograms per cubic metre (ug/m3)"
+)
+PM10 = Gauge(
+    "PM10", "Particulate Matter of diameter less than 10 microns. Measured in micrograms per cubic metre (ug/m3)"
+)
 
-OXIDISING_HIST = Histogram("oxidising_measurements", "Histogram of oxidising measurements", buckets=(0, 10000, 15000, 20000, 25000, 30000, 35000, 40000, 45000, 50000, 55000, 60000, 65000, 70000, 75000, 80000, 85000, 90000, 100000))
-REDUCING_HIST = Histogram("reducing_measurements", "Histogram of reducing measurements", buckets=(0, 100000, 200000, 300000, 400000, 500000, 600000, 700000, 800000, 900000, 1000000, 1100000, 1200000, 1300000, 1400000, 1500000))
-NH3_HIST = Histogram("nh3_measurements", "Histogram of nh3 measurements", buckets=(0, 10000, 110000, 210000, 310000, 410000, 510000, 610000, 710000, 810000, 910000, 1010000, 1110000, 1210000, 1310000, 1410000, 1510000, 1610000, 1710000, 1810000, 1910000, 2000000))
+OXIDISING_HIST = Histogram(
+    "oxidising_measurements",
+    "Histogram of oxidising measurements",
+    buckets=(
+        0,
+        10000,
+        15000,
+        20000,
+        25000,
+        30000,
+        35000,
+        40000,
+        45000,
+        50000,
+        55000,
+        60000,
+        65000,
+        70000,
+        75000,
+        80000,
+        85000,
+        90000,
+        100000,
+    ),
+)
+REDUCING_HIST = Histogram(
+    "reducing_measurements",
+    "Histogram of reducing measurements",
+    buckets=(
+        0,
+        100000,
+        200000,
+        300000,
+        400000,
+        500000,
+        600000,
+        700000,
+        800000,
+        900000,
+        1000000,
+        1100000,
+        1200000,
+        1300000,
+        1400000,
+        1500000,
+    ),
+)
+NH3_HIST = Histogram(
+    "nh3_measurements",
+    "Histogram of nh3 measurements",
+    buckets=(
+        0,
+        10000,
+        110000,
+        210000,
+        310000,
+        410000,
+        510000,
+        610000,
+        710000,
+        810000,
+        910000,
+        1010000,
+        1110000,
+        1210000,
+        1310000,
+        1410000,
+        1510000,
+        1610000,
+        1710000,
+        1810000,
+        1910000,
+        2000000,
+    ),
+)
 
-PM1_HIST = Histogram("pm1_measurements", "Histogram of Particulate Matter of diameter less than 1 micron measurements", buckets=(0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100))
-PM25_HIST = Histogram("pm25_measurements", "Histogram of Particulate Matter of diameter less than 2.5 micron measurements", buckets=(0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100))
-PM10_HIST = Histogram("pm10_measurements", "Histogram of Particulate Matter of diameter less than 10 micron measurements", buckets=(0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100))
+PM1_HIST = Histogram(
+    "pm1_measurements",
+    "Histogram of Particulate Matter of diameter less than 1 micron measurements",
+    buckets=(0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100),
+)
+PM25_HIST = Histogram(
+    "pm25_measurements",
+    "Histogram of Particulate Matter of diameter less than 2.5 micron measurements",
+    buckets=(0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100),
+)
+PM10_HIST = Histogram(
+    "pm10_measurements",
+    "Histogram of Particulate Matter of diameter less than 10 micron measurements",
+    buckets=(0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100),
+)
 
 # Setup InfluxDB
 # You can generate an InfluxDB Token from the Tokens Tab in the InfluxDB Cloud UI
@@ -82,6 +174,7 @@ influxdb_api = influxdb_client.write_api(write_options=SYNCHRONOUS)
 
 # Setup Luftdaten
 LUFTDATEN_TIME_BETWEEN_POSTS = int(os.getenv("LUFTDATEN_TIME_BETWEEN_POSTS", "30"))
+
 
 # Sometimes the sensors can't be read. Resetting the i2c
 def reset_i2c():
@@ -210,7 +303,9 @@ def post_to_influxdb():
         data_points = []
         sensor_data = collect_all_data()
         for field_name in sensor_data:
-            data_points.append(Point(INFLUXDB_MEASUREMENT).tag("node", SENSOR_UID).field(field_name, sensor_data[field_name]))
+            data_points.append(
+                Point(INFLUXDB_MEASUREMENT).tag("node", SENSOR_UID).field(field_name, sensor_data[field_name])
+            )
         try:
             influxdb_api.write(bucket=INFLUXDB_BUCKET, record=data_points)
             if DEBUG:
@@ -235,7 +330,8 @@ def post_to_luftdaten():
         pm_values = dict(i for i in values.items() if i[0].startswith("P"))
         temperature_values = dict(i for i in values.items() if not i[0].startswith("P"))
         try:
-            response_pin_1 = requests.post("https://api.luftdaten.info/v1/push-sensor-data/",
+            response_pin_1 = requests.post(
+                "https://api.luftdaten.info/v1/push-sensor-data/",
                 json={
                     "software_version": "enviro-plus 0.0.1",
                     "sensordatavalues": [{"value_type": key, "value": val} for key, val in pm_values.items()],
@@ -248,7 +344,8 @@ def post_to_luftdaten():
                 },
             )
 
-            response_pin_11 = requests.post("https://api.luftdaten.info/v1/push-sensor-data/",
+            response_pin_11 = requests.post(
+                "https://api.luftdaten.info/v1/push-sensor-data/",
                 json={
                     "software_version": "enviro-plus 0.0.1",
                     "sensordatavalues": [{"value_type": key, "value": val} for key, val in temperature_values.items()],
@@ -288,13 +385,49 @@ def str_to_bool(value):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-b", "--bind", metavar="ADDRESS", default="0.0.0.0", help="Specify alternate bind address [default: 0.0.0.0]")
-    parser.add_argument("-p", "--port", metavar="PORT", default=8000, type=int, help="Specify alternate port [default: 8000]")
-    parser.add_argument("-f", "--factor", metavar="FACTOR", type=float, help="The compensation factor to get better temperature results when the Enviro+ pHAT is too close to the Raspberry Pi board")
-    parser.add_argument("-e", "--enviro", metavar="ENVIRO", type=str_to_bool, help="Device is an Enviro (not Enviro+) so don't fetch data from gas and particulate sensors as they don't exist")
-    parser.add_argument("-d", "--debug", metavar="DEBUG", type=str_to_bool, help="Turns on more verbose logging, showing sensor output and post responses [default: false]")
-    parser.add_argument("-i", "--influxdb", metavar="INFLUXDB", type=str_to_bool, default="false", help="Post sensor data to InfluxDB [default: false]")
-    parser.add_argument("-l", "--luftdaten", metavar="LUFTDATEN", type=str_to_bool, default="false", help="Post sensor data to Luftdaten [default: false]")
+    parser.add_argument(
+        "-b", "--bind", metavar="ADDRESS", default="0.0.0.0", help="Specify alternate bind address [default: 0.0.0.0]"
+    )
+    parser.add_argument(
+        "-p", "--port", metavar="PORT", default=8000, type=int, help="Specify alternate port [default: 8000]"
+    )
+    parser.add_argument(
+        "-f",
+        "--factor",
+        metavar="FACTOR",
+        type=float,
+        help="The compensation factor to get better temperature results when the Enviro+ pHAT is too close to the Raspberry Pi board",
+    )
+    parser.add_argument(
+        "-e",
+        "--enviro",
+        metavar="ENVIRO",
+        type=str_to_bool,
+        help="Device is an Enviro (not Enviro+) so don't fetch data from gas and particulate sensors as they don't exist",
+    )
+    parser.add_argument(
+        "-d",
+        "--debug",
+        metavar="DEBUG",
+        type=str_to_bool,
+        help="Turns on more verbose logging, showing sensor output and post responses [default: false]",
+    )
+    parser.add_argument(
+        "-i",
+        "--influxdb",
+        metavar="INFLUXDB",
+        type=str_to_bool,
+        default="false",
+        help="Post sensor data to InfluxDB [default: false]",
+    )
+    parser.add_argument(
+        "-l",
+        "--luftdaten",
+        metavar="LUFTDATEN",
+        type=str_to_bool,
+        default="false",
+        help="Post sensor data to Luftdaten [default: false]",
+    )
     args = parser.parse_args()
 
     # Start up the server to expose the metrics.
@@ -306,17 +439,29 @@ if __name__ == "__main__":
         DEBUG = True
 
     if args.factor:
-        logging.info("Using compensating algorithm (factor={}) to account for heat leakage from Raspberry Pi board".format(args.factor))
+        logging.info(
+            "Using compensating algorithm (factor={}) to account for heat leakage from Raspberry Pi board".format(
+                args.factor
+            )
+        )
 
     if args.influxdb:
         # Post to InfluxDB in another thread
-        logging.info("Sensor data will be posted to InfluxDB every {} seconds for the node {}".format(INFLUXDB_TIME_BETWEEN_POSTS, SENSOR_UID))
+        logging.info(
+            "Sensor data will be posted to InfluxDB every {} seconds for the node {}".format(
+                INFLUXDB_TIME_BETWEEN_POSTS, SENSOR_UID
+            )
+        )
         influx_thread = Thread(target=post_to_influxdb)
         influx_thread.start()
 
     if args.luftdaten:
         # Post to Luftdaten in another thread
-        logging.info("Sensor data will be posted to Luftdaten every {} seconds for the UID {}".format(LUFTDATEN_TIME_BETWEEN_POSTS, SENSOR_UID))
+        logging.info(
+            "Sensor data will be posted to Luftdaten every {} seconds for the UID {}".format(
+                LUFTDATEN_TIME_BETWEEN_POSTS, SENSOR_UID
+            )
+        )
         luftdaten_thread = Thread(target=post_to_luftdaten)
         luftdaten_thread.start()
 
